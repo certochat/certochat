76f0cb06f2b030a446baeb9e00397f02bf1c6b08
certochat <ali.ahmad@gmx.at>
2025-07-02 19:40:41 +0200

    Setze Flutter-Basis mit WebSocket-Verbindung zu CertoChat-Server (main.dart)

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,1 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+void main() => runApp(CertoChatApp());
+
+class CertoChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CertoChat',
+      home: ChatScreen(
+        channel: IOWebSocketChannel.connect('ws://localhost:3000'),
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  final WebSocketChannel channel;
+  ChatScreen({required this.channel});
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    widget.channel.stream.listen((message) {
+      setState(() {
+        _messages.add(message);
+      });
+    });
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      widget.channel.sink.add(_controller.text);
+      _controller.clear();
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('CertoChat')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: _messages.map((msg) => ListTile(title: Text(msg))).toList(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(child: TextField(controller: _controller)),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: _sendMessage,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
